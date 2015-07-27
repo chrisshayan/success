d08f265af5975e5b4203891d981286392ec0d8ec
Hung H. Nguyen <Hungnh1704@gmail.com>
2015-07-27 16:21:32 +0700

    Add index for improve performance

--- server/startup.js
+++ server/startup.js
@@ -11,4 +11,22 @@ Meteor.startup(function () {
         username: Meteor.settings.mandrill.username,
         key: Meteor.settings.mandrill.key
     });
+
+    /* create index */
+    var options = Meteor.settings.indexMongo._options;
+    var collections = Meteor.settings.indexMongo.collections;
+    
+    if (collections)
+        _.each(collections, function (indexObj, key) {
+            if (Collections[key]) {
+                var col = Collections[key].rawCollection();
+                indexObj.forEach(function (obj) {
+                    col.createIndex(obj, options, function (err, result) {
+                        if (err) throw err;
+                        console.log('indexed : %s !', result)
+                    })
+                })
+            }
+        });
+
 });
