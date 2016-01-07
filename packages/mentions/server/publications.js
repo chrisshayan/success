f1e0c8417019d9eac417072065ba75fbcf079905
Phu Nguyen <mrphu3074>
2016-01-07 09:41:49 +0700

    check permission and scope for mention

--- packages/mentions/server/publications.js
+++ packages/mentions/server/publications.js
@@ -1,7 +1,11 @@
 Meteor.publishComposite('Mentions.Hashtag', function (selector, options) {
 	check(selector, Object);
 	check(options, Object);
+	if(!this.userId) return null;
+	const user = Meteor.users.findOne({_id: this.userId});
+	if(!user || !user.companyId) return null;
 
+	selector['ref.companyId'] = user.companyId;
 	return {
 		find() {
 			if(options['limit']) options['limit'] += 1;
